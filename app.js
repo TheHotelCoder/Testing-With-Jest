ed822929e74ee729f22922f70df9884219bfb590
Diana Ware <dianasofia.ware@gmail.com>
2021-05-04 18:14:35 +0000

    Removing some unnnecesary comments

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const sum = (a,b) => {
 // just a console log for ourselves.
 console.log(sum(7,3))
 
-// export the function to be used on other files 
-// (similar to the keyword `export` when using webpack)
-
-
 // declaramos una funcion con el mismo nombre "formEuroToDollar"
 const fromEuroToDollar = function(valueInEuro){
     // convertimos el valor a dolares
@@ -18,22 +14,15 @@ const fromEuroToDollar = function(valueInEuro){
     return valueInDollar;
 }
 
-const fromDollarToYen = function(fromEuroToDollar){
-    // convertimos el valor a dolares
-    let valueInYen = fromEuroToDollar * 127.9
-    // retornamos el valor
+const fromDollarToYen = function(fromEuroToDollar) {
+    let valueInYen = fromEuroToDollar * 127.9;
     return valueInYen;
 }
 
 const fromYenToPound = function(fromDollarToYen){
-    // convertimos el valor a dolares
-    let valueInPound = fromDollarToYen * 0.8
-    // retornamos el valor
+    let valueInPound = fromDollarToYen * 0.8;
     return valueInPound;
 }
-
-
-
 
 
 module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
--- test.js
+++ test.js
@@ -31,7 +31,6 @@ test("One dollar should be 153.48 yens", function(){
     // utilizo la function de la forma como se espera que funcione
     const yens = fromDollarToYen(3.5)
 
-    // is 1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
     const expected = 3.5 * 127.9; 
     
     // hago mi comparacion (la prueba)
@@ -45,7 +44,7 @@ test("One yen should be 122.784 pounds", function(){
     // utilizo la function de la forma como se espera que funcione
     const pounds = fromYenToPound(3.5)
 
-    // is 1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+
     const expected = 3.5 * 0.8; 
     
     // hago mi comparacion (la prueba)
